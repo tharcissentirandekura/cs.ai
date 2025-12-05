a8d494a2c630e4121539031d3e36bf39d0ca26ee
Tharcisse Ntirandekura <tntirand@oberlin.edu>
2025-12-04 22:30:41 -0500

    Modify file exporting in index.ts

--- sdk/index.ts
+++ sdk/index.ts
@@ -2,8 +2,8 @@
  * Our entry point for sdk
  */
 // core sdk logic 
-export { ChatClient } from './core/client';
-export { ModelManager } from './core/models';
+export * from './core/client';
+export * from './core/models';
 
 // sdk types
 export type * from './types/chat';
